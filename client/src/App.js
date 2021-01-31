a37d324087a9e95daa5ab1ea7bb23644b77accb5
Chris Giardina <chris.giardina@privitar.com>
2021-01-31 14:23:04 -0500

    fixed app title v4

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,14 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> Fibonocci Calc</h1>
+          <h1 className="App-title">Fib Calc</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fibonacci Calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
